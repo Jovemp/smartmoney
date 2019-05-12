01eddd747e1610c9273c3dad9e189a3857caf417
Paulo <jovemp@hotmail.com>
2019-05-11 22:52:47 -0300

    Adicionado banco de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { SQLite } from '@ionic-native/sqlite';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { NewEntryPageModule } from '../pages/new-entry/new-entry.module';
@@ -26,6 +28,7 @@ import { NewEntryPageModule } from '../pages/new-entry/new-entry.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SQLite,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { NewEntryPage } from '../new-entry/new-entry';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +10,36 @@ import { NewEntryPage } from '../new-entry/new-entry';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public sqlite: SQLite) {
 
   }
 
   addEntry() {
     console.log('Adicionar lançamento');
     this.navCtrl.push(NewEntryPage);
+  }
+
+  
+
+  testeDB() {
+    console.log('Inicio de Teste DB');
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      console.log('DB criado');
+      db.sqlBatch([
+        "create table if not exists entries (id integer primary key autoincrement, amount decimal, description text)"
+      ])
+      .then(() => {
+        console.log('tabela criada');
+      })
+      .catch(() => {
+        console.log('Erro no SQL')
+      })
+    })
+    .catch(() => console.log('Erro ao criar bd'))
+
   }
 }
