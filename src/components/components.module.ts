2d3948fbc27aa5bdedee519d408d264170efab82
Paulo <jovemp@hotmail.com>
2019-05-17 16:22:55 -0300

    Foi ajustado o Layout do aplicativo Grafico 1

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,18 +4,21 @@ import { BalancePanelComponent } from './balance-panel/balance-panel';
 import { EntrySummaryComponent } from './entry-summary/entry-summary';
 import { EntryListComponent } from './entry-list/entry-list';
 import { BalanceLabelComponent } from './balance-label/balance-label';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart';
 
 @NgModule({
 	declarations: [BalancePanelComponent,
     EntrySummaryComponent,
     EntryListComponent,
-    BalanceLabelComponent],
+    BalanceLabelComponent,
+    DoughnutChartComponent],
 	imports: [
 		IonicPageModule
 	],
 	exports: [BalancePanelComponent,
     EntrySummaryComponent,
     EntryListComponent,
-    BalanceLabelComponent]
+    BalanceLabelComponent,
+    DoughnutChartComponent]
 })
 export class ComponentsModule {}
